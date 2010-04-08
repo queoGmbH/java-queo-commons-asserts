ce891ace75a6a33e7ae6ccc2aaa5fc5253408563
Ralph Engelmann <r.engelmann@queo-group.com>
2010-04-08 12:17:41 +0000

    Contains mehtoden als Alias von ContainsAtLeast ausgebaut

---
+++ src/test/java/com/queomedia/commons/asserts/AssertUtilTest.java
@@ -0,0 +1,48 @@
+package com.queomedia.commons.asserts;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import junit.framework.ComparisonFailure;
+
+import org.junit.Test;
+
+public class AssertUtilTest {
+
+    @Test
+    public void testEqualsWithoutWhitespace() {
+        AssertUtil.equalsWithoutWhitespace(" hallo", "hallo ");
+    }
+
+    @Test(expected = ComparisonFailure.class)
+    public void testEqualsWithoutWhitespace_notEqual() {
+        AssertUtil.equalsWithoutWhitespace(" haallo", "hallo");
+    }
+
+    @Test
+    public void testContainsExact() {
+        AssertUtil.containsExact(Integer.valueOf(15), Arrays.asList(Integer.valueOf(15)));
+    }
+
+    @Test(expected = ComparisonFailure.class)
+    public void testContainsExact_empty() {
+
+        AssertUtil.containsExact(Integer.valueOf(15), new ArrayList<Integer>());
+
+    }
+
+    @Test(expected = ComparisonFailure.class)
+    public void testContainsExact_wrongItem() {
+
+        AssertUtil.containsExact(Integer.valueOf(15), Arrays.asList(Integer.valueOf(14)));
+
+    }
+
+    @Test(expected = ComparisonFailure.class)
+    public void testContainsExact_tooMutchItems() {
+
+        AssertUtil.containsExact(Integer.valueOf(15), Arrays.asList(Integer.valueOf(15), Integer.valueOf(14)));
+
+    }
+
+}
