f5779db7c0e74efe734629ae4211d415e98e042b
Ralph Engelmann <r.engelmann@queo-group.com>
2015-04-14 16:22:39 +0000

    Junit Test implementiert, aber noch nicht dessen lösung

--- src/test/java/com/queomedia/commons/asserts/AssertUtilTest.java
+++ src/test/java/com/queomedia/commons/asserts/AssertUtilTest.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 
 import junit.framework.ComparisonFailure;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class AssertUtilTest {
@@ -29,23 +30,50 @@ public class AssertUtilTest {
     public void testContainsExact_empty() {
 
         AssertUtil.containsExact(Integer.valueOf(15), new ArrayList<Integer>());
-
     }
 
     @Test(expected = ComparisonFailure.class)
     public void testContainsExact_wrongItem() {
 
         AssertUtil.containsExact(Integer.valueOf(15), Arrays.asList(Integer.valueOf(14)));
-
     }
 
     @Test(expected = ComparisonFailure.class)
     public void testContainsExact_tooMutchItems() {
 
         AssertUtil.containsExact(Integer.valueOf(15), Arrays.asList(Integer.valueOf(15), Integer.valueOf(14)));
-
+    }
+    
+    /**
+     * given two equals lists: with the following character
+     * - both have the same size
+     * - "found" contains ALL objects that are in "expected"
+     * - the items are in different order (this is legal for containsExact)
+     * - they contains the same elements
+     * 
+     * expected: 1,1,2
+     * found:    1,1,2
+     */
+    @Test
+    public void testContainsExact_withSameDoubleItems() {
+        AssertUtil.containsExact(Arrays.asList(1, 1, 2), Arrays.asList(1, 1, 2));
     }
 
+    /**
+     * given two different lists: with the following character
+     * - both have the same size
+     * - "found" contains ALL objects that are in "expected"
+     * - but they are different!
+     * 
+     * expected: 1,1,2
+     * found:    1,2,2
+     */
+    @Ignore
+    @Test(expected = ComparisonFailure.class)
+    public void testContainsExact_withDifferntDoubleItems() {
+        AssertUtil.containsExact(Arrays.asList(1, 1, 2), Arrays.asList(1, 2, 2));
+    }
+    
     private static class RefletionObject {
         @SuppressWarnings("unused")
         private String content;
@@ -53,8 +81,8 @@ public class AssertUtilTest {
         public RefletionObject(String content) {
             this.content = content;
         }
-
     }
+  
 
     /**
      * Test assert reflectiv equals.
